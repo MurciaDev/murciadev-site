21c43201aa41bc97b521a4aabdd0c4318d84a98b
Juan Carlos Ruiz <klaufel@gmail.com>
2022-07-22 13:57:49 +0200

    add seo metas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
-import Head from 'next/head';
 import ContainerHome from '../containers/home';
 
 export default function PageHome() {
-  return (
-    <>
-      <Head>
-        <title>MurciaDev</title>
-        <meta
-          name="description"
-          content="Apasionados del desarrollo frontend y de las tecnologías web en Murcia y alrededores."
-        />
-      </Head>
-      <ContainerHome />
-    </>
-  );
+  return <ContainerHome />;
 }

011b8d7d97e61a3812ee9730092a9a318b14fd6a
Juan Carlos Ruiz <klaufel@gmail.com>
2022-07-22 13:41:10 +0200

    initial commit

--- components/README.md
+++
@@ -1,5 +0,0 @@
-# Components
-
-```node
-pnpm run components:generate <component-name>
-```
--- components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Button } from '@murciadev/ui-button';
-export { default as List } from '@murciadev/ui-list';

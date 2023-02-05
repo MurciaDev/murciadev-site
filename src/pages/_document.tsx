c95dad5c9b369e391dfbc121098bbd591c857d7f
Gabriel Perales <gabriel.perales@cabify.com>
2023-02-05 19:31:51 +0100

    added beamanalytics token

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const BEAMANALYTICS_TOKEN = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -9,6 +11,13 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Jost:wght@400;600&display=swap"
             rel="stylesheet"
           />
+          {BEAMANALYTICS_TOKEN && (
+            <script
+              src="https://beamanalytics.b-cdn.net/beam.min.js"
+              data-token={BEAMANALYTICS_TOKEN}
+              async
+            ></script>
+          )}
         </Head>
         <body>
           <Main />
